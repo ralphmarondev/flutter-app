9696eca9d1542e86b4a613a36624e8e9210c9197
ralphmaron <ralphmaron.dev@gmail.com>
2024-05-04 19:36:08 +0800

    re-initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/components/appbar_demo.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Learning Flutter',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-          useMaterial3: true),
-      home: const AppBarDemo(),
-    );
-  }
+  runApp(Container());
 }
