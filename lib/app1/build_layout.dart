3347f6a18ef3da196c3eddf21670ed2a3d6891c6
ralphmaron <ralphmaron.dev@gmail.com>
2024-05-04 19:01:36 +0800

    simple app layout [scaffold, appbar and body]

---
+++ lib/app1/build_layout.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class BuildLayoutApp extends StatelessWidget {
+  const BuildLayoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Build layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
+// Adding the title section
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                location,
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          )),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41')
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/app1/build_layout.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Courier New'),
-      home: HomePage(),
-    );
-  }
-}
+void main() => runApp(const BuildLayoutApp());
